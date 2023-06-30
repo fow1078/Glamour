7cda9df584a8dc33557dcad90df8ef9a7567ff01
Kolesnik Vlad <fow1078@gmail.com>
2023-07-01 01:13:29 +0300

    Small fix

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask import jsonify
 from flask_cors import CORS, cross_origin
 
@@ -8,10 +8,18 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-@app.route('/api/data')
+@app.route('/api/data', methods=['POST', 'OPTIONS'])
 def get_data():
-    data = {'message': 'This is the data from the backend'}
-    return jsonify(data)
+    if request.method == 'OPTIONS':
+        response = jsonify({'message': 'CORS preflight request successful'})
+        response.headers.add('Access-Control-Allow-Methods', 'POST')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        return response
+
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        return jsonify(data)
 
 
 if __name__ == '__main__':
