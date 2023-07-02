b975bf90ea6b5a375b53a8e70b400b971451232e
Nikkkt <work.terpilovsky@gmail.com>
2023-07-02 11:18:59 +0300

    Order and Support functionality and DB fixes

--- api/api.py
+++ api/api.py
@@ -1,7 +1,7 @@
 import json
 
 from flask import Flask, jsonify
-from functions import generate_item_ID
+from functions import generate_ID
 from flask import request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -12,11 +12,13 @@ cors = CORS(app)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///item.db'
-app.config['SQLALCHEMY_BINDS'] = {'order': 'sqlite:///order.db'}
+app.config['SQLALCHEMY_BINDS'] = {
+    'order': 'sqlite:///order.db',
+    'support': 'sqlite:///support.db'
+    }
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
 
 class Item(db.Model):
     __tablename__ = 'clothes'
@@ -63,52 +65,84 @@ class Order(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     order_id = db.Column(db.String(15), nullable=False)
-    user_name = db.Column(db.String(110), nullable=False)
-    lastname = db.Column(db.String(70), nullable=False)
     email = db.Column(db.String(100), nullable=False)
-    messenger = db.Column(db.String(15), nullable=False)
-    phone_or_nickname = db.Column(db.String(100), nullable=False)
     country = db.Column(db.String(200), nullable=False)
-    city = db.Column(db.String(200), nullable=False)
+    first_name = db.Column(db.String(110), nullable=False)
+    last_name = db.Column(db.String(70), nullable=False)
     address = db.Column(db.String(75), nullable=False)
-    post_office_details = db.Column(db.String(200), nullable=False)
-    post_index = db.Column(db.Integer, nullable=False)
-    additional_comment = db.Column(db.String(1000))
-    order_title = db.Column(db.String(10000), nullable=False)
-    hidden_total__usd = db.Column(db.String(7), nullable=False)
-    hidden_total__uah = db.Column(db.String(7), nullable=False)
+    address_details = db.Column(db.String(200), nullable=False)
+    city = db.Column(db.String(200), nullable=False)
+    postal_code = db.Column(db.Integer, nullable=False)
+    phone_number = db.Column(db.String(100), nullable=False)
+    price_UAH = db.Column(db.Integer, nullable=False)
+    price_USD = db.Column(db.Integer, nullable=False)
     
-    def __init__(self, order_id, user_name, lastname, email, messenger, phone_or_nickname, country, city, address, post_office_details, post_index, additional_comment, order_title, hidden_total__usd, hidden_total__uah):
+    def __init__(self, order_id, email, country, first_name, last_name, address, address_details, city, postal_code, phone_number, price_UAH, price_USD):
         self.order_id = order_id
-        self.user_name = user_name
-        self.lastname = lastname
         self.email = email
-        self.messenger = messenger
-        self.phone_or_nickname = phone_or_nickname
         self.country = country
-        self.city = city
+        self.first_name = first_name
+        self.last_name = last_name
         self.address = address
-        self.post_office_details = post_office_details
-        self.post_index = post_index
-        self.additional_comment = additional_comment
-        self.order_title = order_title
-        self.hidden_total__usd = hidden_total__usd
-        self.hidden_total__uah = hidden_total__uah
+        self.address_details = address_details
+        self.city = city
+        self.postal_code = postal_code
+        self.phone_number = phone_number
+        self.price_UAH = price_UAH
+        self.price_USD = price_USD
+        
+    def toJSON(self):
+        return {
+            'id': self.id,
+            'order_id': self.order_id,
+            'email': self.email,
+            'country': self.country,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'address': self.address,
+            'address_details': self.address_details,
+            'city': self.city,
+            'postal_code': self.postal_code,
+            'phone_number': self.phone_number,
+            'price_UAH': self.price_UAH,
+            'price_USD': self.price_UAH
+        }
+
+class Support(db.Model):
+    __bind_key__ = 'support'
+    __tablename__ = 'support'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.String(100), nullable=False)
+    comment = db.Column(db.String(100000), nullable=False)
+    
+    def __init__(self, full_name, email, phone_number, comment):
+        self.full_name = full_name
+        self.email = email
+        self.phone_number = phone_number
+        self.comment = comment
+        
+    def toJSON(self):
+        return {
+            'full_name': self.full_name,
+            'email': self.email,
+            'phone_number': self.phone_number,
+            'comment': self.comment
+        }
 
 
 @app.route('/api/data', methods=['POST', 'OPTIONS', 'GET'])
 def data():
     if request.method == 'OPTIONS':
-        # Handle CORS preflight request
         response = jsonify({'message': 'CORS preflight request successful'})
-        print(response)
         response.headers.add('Access-Control-Allow-Methods', 'POST')
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
         return response
 
     if request.method == 'POST':
         data = request.get_json()
-        print(data)
         
         string_sizes = ""
         for size in data['sizes']:
@@ -120,7 +154,7 @@ def data():
             string_images += image
             string_images += ", "
             
-        item = Item(generate_item_ID(),
+        item = Item(generate_ID(),
                     data['label'],
                     data['price']['usd'],
                     data['price']['uah'],
@@ -133,7 +167,7 @@ def data():
         try:
             db.session.add(item)
             db.session.commit()
-            return '0'
+            return 200
         
         except Exception as e:
             return str(e)
@@ -143,6 +177,82 @@ def data():
 def send_data():
     all_data = []
     data = Item.query.order_by(Item.id).all()
+    for i in data:
+        all_data.append(json.dumps(i.toJSON()))
+    return all_data
+
+
+@app.route('/api/order_data', methods=['POST', 'OPTIONS', 'GET'])
+def data():
+    if request.method == 'OPTIONS':
+        response = jsonify({'message': 'CORS preflight request successful'})
+        response.headers.add('Access-Control-Allow-Methods', 'POST')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        return response
+
+    if request.method == 'POST':
+        data = request.get_json()
+            
+        order = Order(generate_ID(),
+                    data['email'],
+                    data['country'],
+                    data['first_name'],
+                    data['last_name'],
+                    data['address'],
+                    data['address_details'],
+                    data['city'],
+                    data['postal_code'],
+                    data['phone_number'],
+                    data['price']['uah'],
+                    data['price']['usd'])
+        
+        try:
+            db.session.add(order)
+            db.session.commit()
+            return 200
+        
+        except Exception as e:
+            return str(e)
+        
+        
+@app.route("/api/send_order_data", methods=['POST', 'OPTIONS', 'GET'])
+def send_data():
+    all_data = []
+    data = Order.query.order_by(Order.id).all()
+    for i in data:
+        all_data.append(json.dumps(i.toJSON()))
+    return all_data
+
+
+@app.route('/api/support_data', methods=['POST', 'OPTIONS', 'GET'])
+def data():
+    if request.method == 'OPTIONS':
+        response = jsonify({'message': 'CORS preflight request successful'})
+        response.headers.add('Access-Control-Allow-Methods', 'POST')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        return response
+
+    if request.method == 'POST':
+        data = request.get_json()
+            
+        support = Support(data['full_name'],
+                    data['email'],
+                    data['phone_number'],
+                    data['comment'])
+        
+        try:
+            db.session.add(support)
+            db.session.commit()
+            return 200
+        
+        except Exception as e:
+            return str(e)
+        
+        
+@app.route("/api/send_support_data", methods=['POST', 'OPTIONS', 'GET'])
+def send_data():
+    all_data = []
+    data = Support.query.order_by(Support.id).all()
     for i in data:
         all_data.append(json.dumps(i.toJSON()))
     return all_data
