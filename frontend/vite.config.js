675e8b7ed0be477a5187f1edf4d015b832c6e476
Kolesnik Vlad <fow1078@gmail.com>
2023-07-10 18:07:20 +0300

    Deploy fixes 10

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,9 +7,5 @@ export default defineConfig({
   build: {
     // generate manifest.json in outDir
     manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: '/src/main.jsx',
-    },
   },
 })
