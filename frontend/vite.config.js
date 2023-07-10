4898b5fecc88a8c9e6a91da9895f967f03834240
Kolesnik Vlad <fow1078@gmail.com>
2023-07-10 17:55:16 +0300

    Deploy fixes 8

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,15 @@
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite';
 
 // vite.config.js
 export default defineConfig({
+  plugins: [react()],
   build: {
     // generate manifest.json in outDir
     manifest: true,
     rollupOptions: {
       // overwrite default .html entry
-      input: 'index.html',
+      input: '/src/main.jsx',
     },
   },
 })
