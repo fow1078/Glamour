9ec9bce2b14ec770a713b42fa975ae3e538b7646
Kolesnik Vlad <fow1078@gmail.com>
2023-07-11 18:03:05 +0300

    Admin Styles Fix

--- frontend/src/Secondary Components/BackgroundVideo.jsx
+++ frontend/src/Secondary Components/BackgroundVideo.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-// import Video from '../assets/images/newDarkenFireBg.mp4';
 
 function BackgroundVideo() {
   return (
     <div id="bgVideo">
-      {/* <source src={Video} autoPlay muted loop  type="video/mp4" /> */}
     </div>
   )
 }
